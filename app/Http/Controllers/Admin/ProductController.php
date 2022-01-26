1efec0263f9382fa307d2f7c3dffd34e39cbebc0
gbelda <gbelda56@gmail.com>
2022-01-26 20:08:04 +0100

    add products pagination

--- app/Http/Controllers/Admin/ProductController.php
+++ app/Http/Controllers/Admin/ProductController.php
@@ -15,7 +15,8 @@ class ProductController extends Controller
      */
     public function index()
     {
-        return view('admin.products.index', ['products' => Product::all()]);
+        $products = Product::paginate(10);
+        return view('admin.products.index', compact('products'));
     }
 
     /**
