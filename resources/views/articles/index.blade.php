10df3fb33d8b04da4a6d46f04c376e045a8e4b58
gbelda <gbelda56@gmail.com>
2022-01-27 18:37:32 +0100

    add category in article index

--- resources/views/articles/index.blade.php
+++ resources/views/articles/index.blade.php
@@ -24,20 +24,23 @@
                 <div class="col">
                     <div class="card card-cover h-100 overflow-hidden text-white bg-dark rounded-5 shadow-lg"
                         style="background-image: url('{{ $article->image }}');">
-                        <div class="d-flex flex-column h-100 p-5 pb-3 text-light text-shadow-1 bg-dark bg-opacity-50">
+                        <div class="d-flex flex-column h-100 p-5 pb-1 text-light text-shadow-1 bg-dark bg-opacity-50">
                             <h2 class="pt-5 mt-5 mb-4 display-6 lh-1 fw-bold">
                                 {{ Str::limit($article->title, 25, '...') }}
                             </h2>
-                            <ul class="d-flex list-unstyled mt-auto">
-                                <li class="me-auto">
+                            <ul class="d-flex list-unstyled mt-auto justify-content-between">
+                                <li>
                                     <a href="{{ route('articles.show', $article->slug) }}" class="icon-link">
                                         See article
                                     </a>
                                 </li>
-                                <li class="d-flex align-items-center">
+
+                                <li>
                                     <small>{{ $article->post_date }}</small>
                                 </li>
                             </ul>
+                            <small
+                                class="text-end">{{ $article->category ? $article->category->name : 'Uncategorized' }}</small>
                         </div>
                     </div>
                 </div>
